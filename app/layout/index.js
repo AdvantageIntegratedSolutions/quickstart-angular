06bc294acd0df91271d8e6cf12a84da305bb5672
cullenjett@gmail.com <cullenjett@gmail.com>
2016-12-31 20:07:12 -0700

    move configured client name/logo code to the layout module

--- app/layout/index.js
+++ app/layout/index.js
@@ -1,5 +1,18 @@
+import appConfig from '../../config/app.config.js';
 import Header from './header.component';
 
 angular
   .module('app.layout', [])
+  .run($rootScope => {
+    let name = appConfig.clientDisplayName;
+    $rootScope.clientDisplayName = name ? name : appConfig.client;
+
+    $rootScope.appLogo = appConfig.appLogo;
+
+    let alt = $rootScope.appLogo.dashboardAlt;
+    $rootScope.appLogo.dashboardAlt = alt ? alt : appConfig.client;
+
+    let icon = $rootScope.appLogo.iconUrl;
+    $rootScope.appLogo.iconUrl = icon ? icon : $rootScope.appLogo.dashboardUrl;
+  })
   .component('appHeader', Header)
--- app/shared/index.js
+++ app/shared/index.js
@@ -1,16 +1,5 @@
-import appConfig from '../../config/app.config.js';
 import quickbase from './quickbase-client';
 
 angular
   .module('app.shared', [])
-  .run($rootScope => {
-    let name = appConfig.clientDisplayName;
-    $rootScope.clientDisplayName = name ? name : appConfig.client;
-    // Logo spec
-    $rootScope.appLogo = quickstartConfig.appLogo;
-    let alt = $rootScope.appLogo.dashboardAlt;
-    $rootScope.appLogo.dashboardAlt = alt ? alt : quickstartConfig.client;
-    let icon = $rootScope.appLogo.iconUrl;
-    $rootScope.appLogo.iconUrl = icon ? icon : $rootScope.appLogo.dashboardUrl;
-  })
   .constant('quickbase', quickbase)
